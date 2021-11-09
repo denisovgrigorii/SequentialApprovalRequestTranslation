23e5250959eca4ced611a6025f928d98b344ae1a
Denisov-g <denisov-g@gaz-is.ru>
2021-11-09 14:01:11 +0300

    новая ветка с оптимизированным кодом

--- venv/change_2_2.py
+++ venv/change_2_2.py
@@ -8,12 +8,11 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 
 
-def main_func():
+# работа с файлом excel
+def excel_file(upload_json_data):
     wb = Workbook()
     ws = wb.active
     separator = '\u2192'  # красивый разделить для excel между ролями
-    with open('SequentialApprovalRequest.json', 'r', encoding='utf-8') as json_file:
-        sequential_approval_request = json.load(json_file)  # cериализация json файла
     # заголовки столбцов + выделение жирным шрифтом
     ws['A1'] = 'Имя информационного ресурса'
     ws['B1'] = 'Этапы согласования'
@@ -22,139 +21,81 @@ def main_func():
     # размеры слобцов
     ws.column_dimensions['A'].width = 50
     ws.column_dimensions['B'].width = 120
-    # переменные для работы с cериализованным json
-    name_ir_list = list(sequential_approval_request.keys())
-    number_name_ir_list = 0
     raw_in_column = 2
-    for i in sequential_approval_request.keys():
+    for keys in upload_json_data.keys():
         number_column_a = 'A{}'.format(raw_in_column)
         number_column_b = 'B{}'.format(raw_in_column)
-        ws['{}'.format(number_column_a)] = i
-        # собираем все роли для одной ИР в строку
-        row_role = "".join(str(sequential_approval_request[name_ir_list[number_name_ir_list]]['stages'])
-                           .replace("[", "").replace("]", "")).replace("'", "").replace(' $', '$').split(",")
-        s = 0
-        # парсим lookup значения переменных
-        mass_for_pars = []
-        for role in row_role:
-            if '$' in role:
-                mass_for_pars.append(role)
-        # fieldName + templates.managed.form.organization\templates.managed.form.is.
-        dict_1 = {}
-        # зависимость имя роли в stages -fieldName
-        dict_2 = {}
-        # замена имени роли на fieldName
-        for role_name in mass_for_pars:
-            field_name_role = sequential_approval_request[i]['roleVariables'][role_name.replace(' ', '')]['fieldName']
-            managed_object = sequential_approval_request[i]['roleVariables']\
-                                                        [role_name.replace(' ', '')]['managedObject']
-            dict_2[role_name] = field_name_role
-            if managed_object == 'managed/is':
-                mass_for_pars[s] = str('templates.managed.form.is.' + field_name_role)
-                dict_1[field_name_role] = mass_for_pars[s]
-            elif managed_object == 'managed/organization':
-                mass_for_pars[s] = str('templates.managed.form.organization.' + field_name_role)
-                dict_1[field_name_role] = mass_for_pars[s]
-            s += 1
-        url_translation_ru = jar_unzip() # если получится реализовать запуск на linux
-        # отправляем парсить и получать значения в функцию
-        if len(mass_for_pars) > 0:
-            return_mass_value = find_value(mass_for_pars,url_translation_ru)
-        # cловарь декодироввных переменных и их fieldName
-            result_dict_value = {}
-            dict_3 = {}
-            # decode полученных переменных
-            for value in return_mass_value:
-                decode_value = decode(return_mass_value[value])
-                result_dict_value[value] = decode_value
-            for value in result_dict_value.keys():
-                if value in dict_1.values() and 'templates.managed.form.organization.' in value:
-                    dict_3[str(value).replace('templates.managed.form.organization.', '')] = result_dict_value[value]
-                elif value in dict_1.values() and 'templates.managed.form.is.' in value:
-                    dict_3[str(value).replace('templates.managed.form.is.', '')] = result_dict_value[value]
-            m = 0
-            value_result_list = [value for value in dict_3.values()]
-            for value in dict_2.keys():
-                dict_2[value] = value_result_list[m]
-                m += 1
-            g = 0
-            # удаляем возможные пробелы в ключах словаря(костыль)
-            new_dict_2 = {}
-            for k, v in dict_2.items():
-                new_dict_2[k.strip()] = v
-            # обновляем list stages c ролями для ИС
-            row_role = [row.strip(' ') for row in row_role]
-            for row in row_role:
-                if row in new_dict_2.keys():
-                    row_role[g] = new_dict_2[row]
-                g +=1
-        # проверяем есть ли линейный - если есть добавляем его в стоблец В
-        try:
-            if sequential_approval_request[name_ir_list[number_name_ir_list]]['managerStage']['isEnabled'] == True:
-                row_role.insert(0, 'Линейный руководитель')
-                if row_role[1] == '':  # проверка что в массиве нет пустых значений
-                    row_role.pop(1)
-                ws['{}'.format(number_column_b)] = '{}' \
-                    .format(separator.join(row_role)).replace('[', '')\
-                    .replace(']', '')\
-                    .replace("'", "")\
-                    .replace('\\n', '')
-            else:
-                ws['{}'.format(number_column_b)] = '{}'\
-                    .format(separator.join(row_role)).replace('[', '')\
-                    .replace(']', '')\
-                    .replace("'", "")\
-                    .replace('\\n', '')
-        except KeyError:
-            ws['{}'.format(number_column_b)] = '{}' \
-                .format(separator.join(row_role))\
-                .replace('[', '').replace(']', '')\
-                .replace("'", "")\
-                .replace('\\n', '')
+        ws['{}'.format(number_column_a)] = keys
+        ws['{}'.format(number_column_b)] = separator.join(upload_json_data[keys])
         raw_in_column += 1
-        number_name_ir_list += 1
     wb.save("Этапы согласования.xlsx")
     print("Файл Этапы согласования.xlsx успешно создан в текущей директории")
 
 
-# поиск значений переменных по файлам translation_ru.properties
-def find_value(name_value_for_pars: list = [], url_translation_ru: str = 'translation_ru.properties') -> list:
-    dict_value = {}
-    if len(name_value_for_pars) > 0:
-        with open(url_translation_ru, 'r') as dict_file:
-            for row in dict_file:
-                if len(dict_value) < len(name_value_for_pars):
-                    # если какое либо значение из массива есть в строке - записать в find_value
-                    find_value = [x for x in name_value_for_pars if x in row]
-                    if find_value:
-                        # удаляем все символы до знака "="
-                        dict_value[find_value[0]] = row.split('=', 1)[1].lstrip()
-            dict_file.close()
-        with open('translation_ru.properties', 'r') as dict_file:
-            for row in dict_file:
-                if len(dict_value) < len(name_value_for_pars):
-                    find_value = [x for x in name_value_for_pars if x in row]
-                    if find_value:
-                        # удаляем все символы до знака "="
-                        dict_value[find_value[0]] = row.split('=', 1)[1].lstrip()
-            dict_file.close()
-    return dict_value
+# работа с файлом универсальной цепочки
+def json_file():
+    upload_data = []
+    upload_json_data = {}
+    with open('tmp//SequentialApprovalRequest.json', 'r', encoding='utf-8') as json_file:
+        sequential_approval_request = json.load(json_file)  # cериализация json файла
+    name_ir_list = list(sequential_approval_request.keys())
+    url_translation_ru = jar_unzip()
+    dictionary = create_dict(url_translation_ru)
+    for name_ir in name_ir_list:
+        excel_list = []
+        if 'managerStage' in sequential_approval_request[name_ir].keys() and \
+                sequential_approval_request[name_ir]['managerStage']['isEnabled'] == True:
+            excel_list.append('Линейный руководитель')
+        for stages in sequential_approval_request[name_ir]['stages']:
+            stage = stages[0]
+            if '$' in stage:
+                field_name = sequential_approval_request[name_ir]['roleVariables'][stage]['fieldName']
+                managed_object = \
+                sequential_approval_request[name_ir]['roleVariables'][stage]['managedObject'].split('/')[1]
+                excel_list.append(decode(dictionary[managed_object + '.' + field_name]))
+            else:
+                excel_list.append(stage)
+        upload_data.append(excel_list)
+        upload_json_data[name_ir] = excel_list
+    excel_file(upload_json_data)
 
 
-# decode значений роли
 def decode(unicode_role: str) -> str:
     return unicode_role.encode().decode('unicode-escape')
 
 
+# создание словаря с unicode переменных из файлов транслитерации
+def create_dict(url_translation_ru: str = 'translation_ru.properties') -> list:
+    dict_value = {}
+    in_symbol = 'templates.managed.form.'
+    out_symbol = 'placeholder'
+    with open(url_translation_ru, 'r') as dict_file:
+        for row in dict_file:
+            # если какое либо значение из массива есть в строке - записать в find_value
+            if in_symbol in row and out_symbol not in row:
+                # удаляем все символы до знака "="
+                dict_value[row.split('=')[0].replace(in_symbol, '')] = row.split('=')[1].strip()
+        dict_file.close()
+    with open('tmp//translation_ru.properties', 'r') as dict_file:
+        for row in dict_file:
+            for row in dict_file:
+                # если какое либо значение из массива есть в строке - записать в find_value
+                if in_symbol in row and out_symbol not in row:
+                    # удаляем все символы до знака "="
+                    dict_value[row.split('=')[0].replace(in_symbol, '')] = row.split('=')[1].strip()
+        dict_file.close()
+    return dict_value
+
+
 # функция вытаскивает из архива если получится реализовать запуск на linux
 def jar_unzip() -> str:
-    archive = zipfile.ZipFile('integration-bundle.jar', 'r')
-    archive.extractall('tmp_script')
+    archive = zipfile.ZipFile('tmp//integration-bundle.jar', 'r')
+    archive.extractall('tmp//tmp_script')
     archive.close()
-    return 'tmp_script//i18n//translation_ru.properties'
+    return 'tmp//tmp_script//i18n//translation_ru.properties'
 
 
-# подключение к серверу по SSH
+# подключение к серверу и отрпвка
 def ssh_connect(server_ip, login, password):
     port = 22
     transport = paramiko.Transport((server_ip, port))
@@ -165,9 +106,9 @@ def ssh_connect(server_ip, login, password):
     remote_file_with_ui = '/opt/ankey/ankey/ui/default/ng/public/i18n/translation_ru.properties'
     remote_file_with_sequential_approval_request = '/opt/ankey/ankey/conf/SequentialApprovalRequest.json'
 
-    local_file_with_extensions = 'integration-bundle.jar'
-    local_file_with_ui = 'translation_ru.properties'
-    local_file_with_sequential_approval_request = 'SequentialApprovalRequest.json'
+    local_file_with_extensions = 'tmp\\integration-bundle.jar'
+    local_file_with_ui = 'tmp\\translation_ru.properties'
+    local_file_with_sequential_approval_request = 'tmp\\SequentialApprovalRequest.json'
 
     sftp.get(remote_file_with_extensions, local_file_with_extensions)
     sftp.get(remote_file_with_ui, local_file_with_ui)
@@ -180,16 +121,15 @@ def ssh_connect(server_ip, login, password):
     transport.close()
 
 
+# создание временной директории для файлов полученных по sftp
+def create_tmp_dir():
+    os.mkdir('tmp')
+
+
 # удаление временных файлов\папок
 def remove_tmp_dir():
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp_script')
-    file1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'integration-bundle.jar')
-    file2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'SequentialApprovalRequest.json')
-    file3 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'translation_ru.properties')
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp')
     shutil.rmtree(path)
-    os.remove(file1)
-    os.remove(file2)
-    os.remove(file3)
 
 
 # определение имени интеграционного бандла
@@ -198,7 +138,8 @@ def find_name_bundle():
 
 
 if __name__ == '__main__':
+    create_tmp_dir()
     ssh_connect(server_ip=input('IP address server Ankey IDM: '), login=input('login: '), password=getpass.getpass())
-    main_func()
+    json_file()
     remove_tmp_dir()
 
